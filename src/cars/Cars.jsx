ee57ec2479fa1b4a09d58d2d10af21b1f2c31f06
manishyuchil@gmail.com <manishyuchil@gmail.com>
2022-12-01 17:51:55 +0530

    lorem added

--- src/cars/Cars.jsx
+++ src/cars/Cars.jsx
@@ -6,6 +6,13 @@ function Cars({Brand,color,Price,Model,Engine}) {
             <h2>
                 My Car Name brand is {Brand} and color is {color} also the price is {Price} model of the car is{Model} it has {Engine}
             </h2>
+
+
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur similique, adipisci sequi ratione eveniet corporis iusto voluptatem tempora necessitatibus commodi quibusdam, magnam dolorum excepturi sint, ab nobis rerum dignissimos eos vero. Quam reprehenderit pariatur impedit animi. Repellendus, possimus necessitatibus? Inventore architecto nesciunt totam quo suscipit, temporibus debitis mollitia sit eius eum ullam quaerat sequi delectus hic. Sint possimus odio quas sequi incidunt officia fugiat! Doloribus iure, qui consequuntur voluptatem, deleniti atque saepe dolores incidunt voluptate commodi excepturi illo facere architecto omnis recusandae amet deserunt esse voluptatum blanditiis quae. Tempora iure soluta dolores non aliquam incidunt nesciunt alias vel quam consectetur!</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
     
