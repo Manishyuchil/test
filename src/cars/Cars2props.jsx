86c8a76e587dfae790bdada85c944a3011688ba3
manishyuchil@gmail.com <manishyuchil@gmail.com>
2022-12-01 17:50:59 +0530

    changes 5 div

--- src/cars/Cars2props.jsx
+++ src/cars/Cars2props.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const  CarNew = (props) =>{
 return(
     <div>
+
+        <div>jmdskfkasdkfka</div>
+        <div> sfmcasmcnmsanasm</div>
+        <div>asfmkasfmkl</div>
+        <div>n mknadsk</div>
+        <div>n mkaskm</div>
         
         <h2>
         My Car brand is {props.Brand} and color is {props.color} also the price is {props.Price} model of the car is{props.Model} it has {props.Engine}
